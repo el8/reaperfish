c40e452659f2dab237b39257c7fc38552f97f0a2
Jan Glauber <jglauber@digitalocean.com>
2022-04-22 14:28:59 +0200

    bpf: remove crash on kernel version test
    New idea to avoid this pile of crap: why not make a bpf map from
    golang and simply put the kernel version there?

--- src/bpf/reaper-log.bpf.c
+++ src/bpf/reaper-log.bpf.c
@@ -88,8 +88,6 @@ int BPF_PROG(trace_bio_start, struct bio *bio)
 	u64 ts = bpf_ktime_get_ns();
 	u64 len;
 
-	bpf_printk("kernel version: %d\n", LINUX_KERNEL_VERSION);
-
 	bpf_map_update_elem(&bio_start, &bio, &ts, 0);
 
 	struct taskinfo_t ti = {};
